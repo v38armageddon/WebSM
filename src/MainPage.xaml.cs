a5ea0aa062a579a8afd6216fd7a980af19098b3a
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-10-13 18:11:04 +0200

    Add ProgressRing and remove forced square UI

--- src/MainPage.xaml.cs
+++ src/MainPage.xaml.cs
@@ -44,6 +44,7 @@ namespace WebSM
 
         private void webView2_NavigationStarting(Microsoft.UI.Xaml.Controls.WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs args)
         {
+            progressRing.IsActive = true;
             // Fix Google connection blocked due to UserAgent, see https://github.com/MicrosoftEdge/WebView2Feedback/issues/1647
             var settings = webView2.CoreWebView2.Settings;
             if (webView2.Source.ToString().Contains("https://accounts.google.com"))
@@ -58,6 +59,7 @@ namespace WebSM
 
         private void webView2_NavigationCompleted(WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs args)
         {
+            progressRing.IsActive = false;
             GC.Collect();
             GC.WaitForPendingFinalizers();
         }
@@ -71,7 +73,6 @@ namespace WebSM
         {
             return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edge/103.0.1264.77";
         }
-
 
         private void navView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
         {
