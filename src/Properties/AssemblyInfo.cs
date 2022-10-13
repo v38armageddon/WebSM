9884ee4609183392a713d38f25deb7c5cbf79fac
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-10-13 18:16:09 +0200

    Change version and min version Win10 1804 => 2004

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("WebSM")]
-[assembly: AssemblyDescription("UWP App who you can link your Social Media.")]
+[assembly: AssemblyDescription("UWP App who you can have your Social Media in one application.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("v38armageddon")]
 [assembly: AssemblyProduct("WebSM")]
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
