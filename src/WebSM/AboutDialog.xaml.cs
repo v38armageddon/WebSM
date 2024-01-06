8ca9653b9fe0ddc03dda2322e6b9e90ac31ba97e
v38armageddon <v38armageddon@outlook.com>
2024-01-06 15:28:14 +0100

    accidentaly deleted WebSM

---
+++ src/WebSM/AboutDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Store;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace WebSM
+{
+    public sealed partial class AboutDialog : ContentDialog
+    {
+        public AboutDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            bool result = await Launcher.LaunchUriAsync(new Uri("ms-windows-store://review/?ProductId=9NVMBH7W0HXF"));
+            result = true;
+        }
+    }
+}
